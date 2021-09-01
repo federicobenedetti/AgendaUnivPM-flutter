475e7ca472bde6515cead58231d227a31b799002
Federico Benedetti <fede.benedetti9@gmail.com>
2021-09-01 19:07:34 +0200

    Navigation bar now working

--- lib/screens/dettagliocorso.dart
+++ lib/screens/dettagliocorso.dart
@@ -11,7 +11,17 @@ class _DettaglioCorsoState extends State<DettaglioCorso> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Agenda UnivPM"),
+      ),
+      body: Padding(
+      padding: EdgeInsets.all(15.0),
+        child: Column(
+          children: <Widget>[
 
+          ],
+        ),
+      )
     ); // Scaffold
   }
 }
--- lib/screens/userprofile.dart
+++ lib/screens/userprofile.dart
@@ -9,23 +9,104 @@ class UserProfile extends StatefulWidget {
 }
 
 class _UserProfileState extends State<UserProfile> {
+  int _currentIndex = 0;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Agenda UnivPM"),
+  static List<Widget> _pages = <Widget>[
+    Container(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            width: double.infinity,
+            child: Container(
+              child: Text(
+                  "Profilo",
+                  textAlign: TextAlign.left,
+                  style:
+                  TextStyle(
+                      fontSize: 22,
+                      color: Colors.black
+                  )
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+              "Questa sezione mostra lo stato della tua carriera universitaria. Potrai quindi visualizzare la tua matricola, inviare un feedback per migliorare l'app o visualizzare le FAQ per ulteriori chiarimenti."
+          ),
+          SizedBox(height: 20),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+                child: Row(
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 50.0,
+                        child: ClipRRect(
+                          child: Text("MR",
+                              style:
+                              TextStyle(
+                                  fontSize: 38
+                              )),
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text("Mario Rossi",
+                            textAlign: TextAlign.left,
+                            style:
+                            TextStyle(
+                                fontSize: 28
+                            ),
+                          ),
+                          Text("S123456",
+                            textAlign: TextAlign.left,
+                          )
+                        ],
+                      )
+                    ]
+                )
+            ),
+          ),
+        ],
       ),
-      body: Padding(
-        padding: EdgeInsets.all(15.0),
+    ),
+
+    Container(
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            width: double.infinity,
+            child: Container(
+              child: Text(
+                  "Corsi a cui puoi iscriverti",
+                  textAlign: TextAlign.left,
+                  style:
+                  TextStyle(
+                      fontSize: 22,
+                      color: Colors.black
+                  )
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            "Questa sezione mostra la lista di tutti i corsi disponibili a cui puoi iscriverti."
+          ),
+          SizedBox(height: 20),
+        ],
+      )
+    ),
+
+    Container(
         child: Column(
           children: <Widget>[
             SizedBox(
               width: double.infinity,
               child: Container(
                 child: Text(
-                  "Profilo",
-                  textAlign: TextAlign.left,
+                    "Corsi a cui sei iscritto",
+                    textAlign: TextAlign.left,
                     style:
                     TextStyle(
                         fontSize: 22,
@@ -36,46 +117,49 @@ class _UserProfileState extends State<UserProfile> {
             ),
             SizedBox(height: 10),
             Text(
-              "Questa sezione mostra lo stato della tua carriera universitaria. Potrai quindi visualizzare la tua matricola, inviare un feedback per migliorare l'app o visualizzare le FAQ per ulteriori chiarimenti."
+              "Questa sezione mostra i corsi a cui sei attualmente iscritto. Entrando nel dettaglio del corso potrai scegliere di disiscriverti dal corso."
             ),
             SizedBox(height: 20),
-            Align(
-              alignment: Alignment.centerLeft,
+          ],
+        )
+    ),
+    
+    Container(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              width: double.infinity,
               child: Container(
-                child: Row(
-                  children: <Widget>[
-                    CircleAvatar(
-                      radius: 50.0,
-                      child: ClipRRect(
-                        child: Text("MR",
-                            style:
-                            TextStyle(
-                                fontSize: 38
-                            )),
-                        borderRadius: BorderRadius.circular(50.0),
-                      ),
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Text("Mario Rossi",
-                            textAlign: TextAlign.left,
-                            style:
-                            TextStyle(
-                                fontSize: 28
-                            ),
-                        ),
-                        Text("S123456",
-                          textAlign: TextAlign.left,
-                        )
-                      ],
+                child: Text(
+                    "Lista lezioni",
+                    textAlign: TextAlign.left,
+                    style:
+                    TextStyle(
+                        fontSize: 22,
+                        color: Colors.black
                     )
-
-                  ]
-                )
+                ),
               ),
             ),
+            SizedBox(height: 10),
+            Text(
+              "Questa sezione mostra l'elenco delle lezioni dei vari corsi a cui sei iscritto. Entrando nel dettaglio potrai accedere allo streaming della lezione."
+            ),
+            SizedBox(height: 20),
           ],
-        ),
+        )
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Agenda UnivPM"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child:  _pages.elementAt(_currentIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
@@ -100,8 +184,15 @@ class _UserProfileState extends State<UserProfile> {
                 backgroundColor: Colors.red
             )
           ],
-          currentIndex: 0,
+          onTap: onTabTapped, // new
+          currentIndex: _currentIndex,
       ),
     ); // Scaffold
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
