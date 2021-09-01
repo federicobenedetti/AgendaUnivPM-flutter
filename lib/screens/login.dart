55c0e92320bb70f6747ce41ae2886e34d70c08c3
Federico Benedetti <fede.benedetti9@gmail.com>
2021-09-01 11:45:35 +0200

    Finish UI for: Login page, init user profile

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,3 +1,4 @@
+import 'package:agendaunivpm_f/screens/userprofile.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -14,44 +15,49 @@ class _LoginState extends State<Login> {
       appBar: AppBar(
         title: Text("Agenda UnivPM"),
       ),
-        body: Center(
-            child: ColoredBox(
-                color: Colors.white,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    SizedBox(height: 50),
-                    Image.asset(
-                      "images/univpm.png",
-                      width: 200,
-                    ),
-                    SizedBox(height: 30),
-                    Text("Agenda UnivPM",
-                        style:
-                        TextStyle(
-                          fontSize: 22,
-                          color: Colors.black
-                        )
-                    ),
-                    SizedBox(height: 50),
-                    ElevatedButton.icon(
-                        onPressed: () {},
-                        icon: Icon(Icons.android),
-                        label: Text("Accedi")
-                    ),
-                    Spacer(),
-                    Text(
-                        'All rights reserved - Copyright Federico Benedetti',
-                        style:
-                        TextStyle(
-                            fontSize: 15,
+        body: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Center(
+              child: ColoredBox(
+                  color: Colors.white,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      SizedBox(height: 50),
+                      Image.asset(
+                        "images/univpm.png",
+                        width: 200,
+                      ),
+                      SizedBox(height: 30),
+                      Text("Agenda UnivPM",
+                          style:
+                          TextStyle(
+                            fontSize: 22,
                             color: Colors.black
-                        )
-                    ),
-                    SizedBox(height: 20)
-                  ],
-                )
-            )
+                          )
+                      ),
+                      SizedBox(height: 50),
+                      ElevatedButton.icon(
+                          onPressed: () {
+                            Navigator.pushReplacementNamed(context, '/userprofile');
+                          },
+                          icon: Icon(Icons.android),
+                          label: Text("Accedi")
+                      ),
+                      Spacer(),
+                      Text(
+                          'All rights reserved - Copyright Federico Benedetti',
+                          style:
+                          TextStyle(
+                              fontSize: 15,
+                              color: Colors.black
+                          )
+                      ),
+                      SizedBox(height: 20)
+                    ],
+                  )
+              )
+          ),
         )
     ); // Scaffold
   }
--- lib/screens/userprofile.dart
+++ lib/screens/userprofile.dart
@@ -13,12 +13,76 @@ class _UserProfileState extends State<UserProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Agenda UnivPM"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              width: double.infinity,
+              child: Container(
+                child: Text(
+                  "Profilo",
+                  textAlign: TextAlign.left,
+                    style:
+                    TextStyle(
+                        fontSize: 22,
+                        color: Colors.black
+                    )
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Questa sezione mostra lo stato della tua carriera universitaria. Potrai quindi visualizzare la tua matricola, inviare un feedback per migliorare l'app o visualizzare le FAQ per ulteriori chiarimenti."
+            ),
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                child: Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 50.0,
+                      child: ClipRRect(
+                        child: Text("MR",
+                            style:
+                            TextStyle(
+                                fontSize: 38
+                            )),
+                        borderRadius: BorderRadius.circular(50.0),
+                      ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text("Mario Rossi",
+                            textAlign: TextAlign.left,
+                            style:
+                            TextStyle(
+                                fontSize: 28
+                            ),
+                        ),
+                        Text("S123456",
+                          textAlign: TextAlign.left,
+                        )
+                      ],
+                    )
+
+                  ]
+                )
+              ),
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
                 icon: Icon(Icons.account_circle),
                 label: 'Profilo',
-                backgroundColor: Colors.red
+                backgroundColor: Colors.red,
             ),
             BottomNavigationBarItem(
                 icon: Icon(Icons.list),
@@ -36,7 +100,7 @@ class _UserProfileState extends State<UserProfile> {
                 backgroundColor: Colors.red
             )
           ],
-          currentIndex: 0
+          currentIndex: 0,
       ),
     ); // Scaffold
   }
